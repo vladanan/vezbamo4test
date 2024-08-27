8c3ffd7b05c80d632f0f6ee1843bcb0095fce775
Vladan <vladan.andjelkovic@gmail.com>
2024-08-27 09:34:18 -0400

    2

--- node/test.js
+++ node/test.js
@@ -1,13 +1,19 @@
-const axios = require('axios')
+// const axios = require('axios')
+import axios from 'axios';
 
-console.log("node let's go")
+let t = new Date()
+let tt = new Date()
+console.log("let's node:", t)
+let url = "http://192.168.2.100:10000/api/v/test/id/2?"
 
 for (let i = 990; i < 1000; i++) {
-	axios.get(`http://192.168.2.100:10000/api/v/test/id/2?${i}`)
-  .then(function (response) {
-		console.log(response.data[0].Area)
+	axios.get(`${url}${i}`)
+	.then(function (response) {
+		console.log(i, new Date() - tt, response.data[0].Area)
+		tt = new Date()
 	})
 	.catch(function (error) {
 		console.log(error)
 	});
 }
+console.log("node done:", new Date() - t)
